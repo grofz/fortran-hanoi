9acfb8b242a80798b9de823c4ad7df3a2cf93655
Zdenek Grof <grofz@vscht.cz>
2026-01-21 06:49:19 +0100

    update build bat in Win

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
- gcc -c wrapper.c
- gfortran -Ofast -J../fortran-snake/fortran-raylib wrapper.o hanoigui.f90 main.f90 -lraylib -lwinmm -lgdi32 -fno-range-check --max-errors=3 -o hanoi.exe
+ gcc -I../raylib/src -c wrapper.c
+ gfortran -Ofast -J../fortran-snake/fortran-raylib ../fortran-snake/fortran-raylib/raylib_util.o wrapper.o hanoigui.f90 main.f90 -lraylib -lwinmm -lgdi32 -fno-range-check --max-errors=3 -o hanoi.exe
